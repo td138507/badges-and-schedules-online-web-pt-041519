37c55fe3fe93b02eac4657a05296bb0886c5b300
Taylor Doyle <tcd56@cornell.edu>
2019-05-08 18:54:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,21 +6,24 @@ end
 people = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
 
 
-def batch_badge_creator(people)
-  badges = []
-  people.each {|people| << badge_maker(people)}
-  badges
+def batch_badge_creator(attendees)
+  attendees.collect do |attendees|
+  "Hello, my name is #{attendees}."
+end
 end
 
-def assign_rooms(people)
-  rooms = []
-  people.each_with_index do |people, index|
-    rooms << "Hello, #{people}! You'll be assigned to room #{index + 1}!"
-  end
-  rooms
+def assign_rooms(attendees)
+  attendees.each_with_index.collect do |attendee, index|
+  "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
+end
 end
 
-def printer(people)
-  batch_badge_creator(people).each {|people| puts attendee}
-  assign_rooms(people).each {|people| puts attendee}
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+  puts badge
+end
+
+assign_rooms(attendees).each do |assignment|
+  puts assignment
+end
 end
